b3dbf4161c0d549ec5dae491f165f5c90d856a7f
Nathaniel Zeeman <nathanielzeeman@gmail.com>
2018-11-20 22:17:39 -0800

    created initial version

---
+++ Assets/Scripts/CubeBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    // public serialized vars
+    public float despawnTime;
+    public float maxInitialVelocity;
+    // private vars
+    private Rigidbody rb;
+
+    void OnEnable()
+    {
+        // initialize rigidbody
+        rb = GetComponent<Rigidbody>();
+
+        // apply random velocity
+        rb.velocity = Random.insideUnitSphere * maxInitialVelocity;
+
+        // wait some time before disappearing
+        StartCoroutine(DespawnTimer());
+    }
+
+    IEnumerator DespawnTimer()
+    {
+        yield return new WaitForSeconds(despawnTime);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public float spawnPeriod;
+
+	void Start ()
+    {
+        // start spawning cubes
+        StartCoroutine(SpawnCycle());
+	}
+
+    IEnumerator SpawnCycle()
+    {
+        while (true)
+        {
+            if (Input.GetMouseButton(0))
+                SpawnCube();
+
+            yield return new WaitForSeconds(spawnPeriod);
+        }
+    }
+
+    void SpawnCube()
+    {
+        GameObject obj = Instantiate(prefab);
+        obj.transform.parent = transform;
+        obj.transform.position = transform.position;
+    }
+}
